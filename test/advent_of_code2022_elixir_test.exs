940b52b7850b49fda3c47c8a6eff37a2ab61831c
s-loar <sjl7909@gmail.com>
2022-12-12 09:48:57 -0500

    Remove un-needed code and tests

--- test/advent_of_code2022_elixir_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AdventOfCode2022ElixirTest do
-  use ExUnit.Case
-  doctest AdventOfCode2022Elixir
-
-  test "greets the world" do
-    assert AdventOfCode2022Elixir.hello() == :world
-  end
-end
