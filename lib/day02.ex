d033c8b83250fadc4b371bc2f0c477cc04b63fe8
s-loar <sjl7909@gmail.com>
2022-12-13 12:57:57 -0500

    Day 02, part 2, solution

--- lib/day02.ex
+++ lib/day02.ex
@@ -7,12 +7,36 @@ defmodule Day02 do
     |> Enum.sum()
   end
 
+  def part_2 do
+    File.read!('./data/day02.txt')
+    |> String.split( "\n", trim: true)
+    |> Enum.map(&expectation_score/1)
+    |> Enum.sum()
+  end
+
+  defp expectation_score(round) do
+    case round do
+      # X is to loose
+      "A X" -> 0 + 3
+      "B X" -> 0 + 1
+      "C X" -> 0 + 2
+      # Y is to tie
+      "A Y" -> 3 + 1
+      "B Y" -> 3 + 2
+      "C Y" -> 3 + 3
+      # Z is to win
+      "A Z" -> 6 + 2
+      "B Z" -> 6 + 3
+      "C Z" -> 6 + 1
+    end
+  end
+
   defp score_for_round(round) do
     hand_score(String.slice(round, 2, 1)) + win_score(round)
   end
 
-  defp hand_score(my_hand) do
-    case my_hand do
+  defp hand_score(hand) do
+    case hand do
       "X" -> 1
       "Y" -> 2
       "Z" -> 3
